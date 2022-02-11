bad7ad88a55dfa6271e1723209816e1baf8c895e
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-11 23:25:57 +0100

    added documents and error variables to getCollection

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,6 +1,9 @@
 import { ref } from "vue";
 import { db } from "../firebase/config";
 
-const getCollection = (collection) => {};
+const getCollection = (collection) => {
+  const documents = ref(null);
+  const error = ref(null);
+};
 
 export default getCollection;
