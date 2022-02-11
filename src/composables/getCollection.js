915d55f1bb92ddb9ca5f58c9a13bae0f7fc438b7
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-11 23:26:45 +0100

    register the db collection reference

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -4,6 +4,9 @@ import { db } from "../firebase/config";
 const getCollection = (collection) => {
   const documents = ref(null);
   const error = ref(null);
+
+  // register the db collection reference
+  let collectionRef = db.collection(collection).orderBy("createdAt");
 };
 
 export default getCollection;
