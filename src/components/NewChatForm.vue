42fac291949abc04bb5fc436be66336c73f0e278
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-11 22:46:41 +0100

    added handlesubmit for newFormChat

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -3,34 +3,53 @@
     <textarea
       placeholder="Type a message and hit enter to send..."
       v-model="message"
+      @keypress.enter.prevent="handleSubmit"
     ></textarea>
-    <div v-if="error" class="error">{{ error }} blah</div>
   </form>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import getUser from "../composables/getUser";
+import { timestamp } from "../firebase/config";
+
+export default {
+  setup() {
+    const message = ref("");
+    const { user } = getUser();
+
+    const handleSubmit = async () => {
+      const chat = {
+        name: user.value.displayName,
+        message: message.value,
+        createdAt: timestamp(),
+      };
+      console.log(chat);
+    };
+    return { message, handleSubmit };
+  },
+};
 </script>
 
 <style scoped>
-  form {
-    margin: 10px;
-  }
-  textarea {
-    width: 100%;
-    max-width: 100%;
-    margin-bottom: 6px;
-    padding: 10px;
-    box-sizing: border-box;
-    border: 0;
-    border-radius: 20px;
-    font-family: inherit;
-    outline: none;
-  }
-  .error {
-    text-align: center;
-    color: #ff2a58;
-    font-size: 12px;
-    padding: 10px 0;
-  }
+form {
+  margin: 10px;
+}
+textarea {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+}
+.error {
+  text-align: center;
+  color: #ff2a58;
+  font-size: 12px;
+  padding: 10px 0;
+}
 </style>
